040056879b93fbe9cc5d252e2f9e585631068b9e
Babafemi <babaphemy@gmail.com>
2021-12-08 12:44:56 -0600

    new theme

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+//import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/css/style.css';
+import './App.css';
+
+const history = createBrowserHistory();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  <Router history={history}>
+    <App />
+  </Router>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
