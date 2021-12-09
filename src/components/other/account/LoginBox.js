32b25cc22fda03f28e3595f146562445926b903b
Babafemi <babaphemy@gmail.com>
2021-12-09 17:02:40 -0600

    connect to api

--- src/components/other/account/LoginBox.js
+++ src/components/other/account/LoginBox.js
@@ -1,87 +1,125 @@
-import React from 'react';
+import React, { useState } from "react";
 import SignInOptions from "./SignInOptions";
-import {AiOutlineUser} from 'react-icons/ai'
-import {FiLock} from 'react-icons/fi'
-import {Link} from "react-router-dom";
+import { AiOutlineUser } from "react-icons/ai";
+import { FiLock } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
-function LoginBox({title, subtitle}) {
-    return (
-        <>
-            <div className="billing-form-item mb-0">
-                <div className="billing-title-wrap border-bottom-0 pr-0 pl-0 pb-0 text-center">
-                    <h3 className="widget-title font-size-28 pb-0">
-                        {title}
-                    </h3>
-                    <p className="font-size-16 font-weight-medium">
-                        {subtitle}
-                    </p>
+function LoginBox({ title, subtitle }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const _submit = async () => {
+    if (email.length > 0 && password.length > 0) {
+      const obj = { email, password };
+      const response = await fetch("http://localhost:5071/api/v1/user/login", {
+        body: JSON.stringify(obj),
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Basic ${process.env.REACT_APP_AUTH}`,
+        },
+      });
+      if (!response.ok) {
+        // login failed
+        alert("Login failed");
+      }
+      const user = await response.json();
+      user?.id && alert("Login succesful!");
+    }
+  };
+  return (
+    <>
+      <div className="billing-form-item mb-0">
+        <div className="billing-title-wrap border-bottom-0 pr-0 pl-0 pb-0 text-center">
+          <h3 className="widget-title font-size-28 pb-0">{title}</h3>
+          <p className="font-size-16 font-weight-medium">{subtitle}</p>
+        </div>
+        <div className="billing-content">
+          <div className="contact-form-action">
+            <div className="row">
+              <SignInOptions />
+
+              <div className="col-lg-12">
+                <div className="account-assist mt-4 mb-4 text-center">
+                  <p className="account__desc">or</p>
                 </div>
-                <div className="billing-content">
-                    <div className="contact-form-action">
-                        <form method="post">
-                            <div className="row">
-
-                                <SignInOptions />
-
-                                <div className="col-lg-12">
-                                    <div className="account-assist mt-4 mb-4 text-center">
-                                        <p className="account__desc">or</p>
-                                    </div>
-                                </div>
-                                <div className="col-lg-12">
-                                    <div className="input-box">
-                                        <label className="label-text">Username, or email</label>
-                                        <div className="form-group">
-                                                <span className="form-icon">
-                                                    <AiOutlineUser />
-                                                </span>
-                                            <input className="form-control" type="email" name="text" placeholder="Username, or email" />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-lg-12">
-                                    <div className="input-box">
-                                        <label className="label-text">Password</label>
-                                        <div className="form-group">
-                                                <span className="form-icon">
-                                                    <FiLock />
-                                                </span>
-                                            <input className="form-control" type="text" name="text" placeholder="Password" />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-lg-12">
-                                    <div className="form-group">
-                                        <div className="custom-checkbox mr-0 d-flex align-items-center justify-content-between">
-                                            <div>
-                                                <input type="checkbox" id="chb1" />
-                                                <label htmlFor="chb1">Remember Me</label>
-                                            </div>
-                                            <div>
-                                                <Link to="/recover" className="color-text font-weight-medium">
-                                                    Forgot password?
-                                                </Link>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-lg-12">
-                                    <div className="btn-box margin-top-20px margin-bottom-20px">
-                                        <button className="theme-btn border-0" type="submit">
-                                            Login now
-                                        </button>
-                                    </div>
-                                </div>
-                                <div className="col-lg-12">
-                                    <p className="font-weight-medium">Not a member? <Link to="/sign-up" className="color-text"> Register</Link></p>
-                                </div>
-                            </div>
-                        </form>
+              </div>
+              <div className="col-lg-12">
+                <div className="input-box">
+                  <label className="label-text">Username, or email</label>
+                  <div className="form-group">
+                    <span className="form-icon">
+                      <AiOutlineUser />
+                    </span>
+                    <input
+                      className="form-control"
+                      type="email"
+                      name="text"
+                      placeholder="Username, or email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-12">
+                <div className="input-box">
+                  <label className="label-text">Password</label>
+                  <div className="form-group">
+                    <span className="form-icon">
+                      <FiLock />
+                    </span>
+                    <input
+                      className="form-control"
+                      type="password"
+                      name="text"
+                      placeholder="Password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-12">
+                <div className="form-group">
+                  <div className="custom-checkbox mr-0 d-flex align-items-center justify-content-between">
+                    <div>
+                      <input type="checkbox" id="chb1" />
+                      <label htmlFor="chb1">Remember Me</label>
                     </div>
+                    <div>
+                      <Link
+                        to="/recover"
+                        className="color-text font-weight-medium"
+                      >
+                        Forgot password?
+                      </Link>
+                    </div>
+                  </div>
                 </div>
+              </div>
+              <div className="col-lg-12">
+                <div className="btn-box margin-top-20px margin-bottom-20px">
+                  <button className="theme-btn border-0" onClick={_submit}>
+                    Login now
+                  </button>
+                </div>
+              </div>
+              <div className="col-lg-12">
+                <p className="font-weight-medium">
+                  Not a member?{" "}
+                  <Link to="/sign-up" className="color-text">
+                    {" "}
+                    Register
+                  </Link>
+                </p>
+              </div>
             </div>
-        </>
-    );
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default LoginBox;
--- src/components/places/PlaceGrid.js
+++ src/components/places/PlaceGrid.js
@@ -1,80 +1,128 @@
-import React from 'react';
-import {IoIosLink} from "react-icons/io";
-import {FiHeart, FiPhone} from "react-icons/fi";
-import {FaRegCalendarCheck} from "react-icons/fa";
-import {AiOutlineEye} from "react-icons/ai";
-import {Link} from "react-router-dom";
-
-function PlaceGrid({griditems}) {
-    return (
-        <>
-            {griditems.map((item, index) => {
-                return (
-                    <div className="col-lg-4 column-td-6" key={index}>
-                        <div className="card-item">
-                            <Link to={item.titleUrl} className="card-image-wrap">
-                                <div className="card-image">
-                                    <img src={item.image} className="card__img" alt="Place" />
-                                    <span className={item.titleIcon ? 'badge': 'badge badge-closed' }>{item.bedge}</span>
-                                    <span className="badge-toggle" data-toggle="tooltip" data-placement="bottom" title="22 Likes">
-                                        <FiHeart />
-                                    </span>
-                                </div>
-                            </Link>
-                            <div className="card-content-wrap">
-                                <div className="card-content">
-                                    <Link to={item.titleUrl}>
-                                        <h5 className="card-meta">
-                                            <span>{item.cardTypeIcon}</span> {item.cardType}
-                                        </h5>
-                                        <h4 className="card-title">{item.title}
-                                            <i>{item.titleIcon}</i>
-                                        </h4>
-                                        <p className="card-sub">
-                                            {item.stitle}
-                                        </p>
-                                    </Link>
-                                    <a href={item.authorUrl} className="author-img">
-                                        <img src={item.author} alt="author-img" />
-                                    </a>
-                                    <ul className="info-list padding-top-20px">
-                                        <li><span className="la d-inline-block"><FiPhone /></span> {item.number}</li>
-                                        <li><span className="la d-inline-block"><IoIosLink /></span>  <a href={item.websiteUrl}>
-                                            {item.website}
-                                        </a>
-                                        </li>
-                                        <li>
-                                            <span className="la d-inline-block"><FaRegCalendarCheck /></span> {item.date}
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div className="rating-row">
-                                    <div className="rating-rating">
-                                        {item.ratings.map((rating, index) => {
-                                            return (
-                                                <span key={index}>{rating}</span>
-                                            )
-                                        })}
-                                        <span className="rating-count">{item.ratingNum}</span>
-                                    </div>
-                                    <div className="listing-info">
-                                        <ul>
-                                            <li><span className="info__count"><AiOutlineEye /></span> {item.view}</li>
-                                            <li>
-                                                <span className="info__save" data-toggle="tooltip" data-placement="top" title="Bookmark">
-                                                    <FiHeart />
-                                                </span>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                )
-            })}
-        </>
-    );
+import React, { useEffect, useState } from "react";
+import { IoIosLink } from "react-icons/io";
+import { FiDollarSign, FiHeart, FiPhone } from "react-icons/fi";
+import { FaRegCalendarCheck } from "react-icons/fa";
+import { AiOutlineEye } from "react-icons/ai";
+import { Link } from "react-router-dom";
+export const auth = {
+  headers: { Authorization: `Basic ${process.env.REACT_APP_AUTH}` },
+};
+function PlaceGrid({ griditems }) {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    async function getData() {
+      const d = await fetch("http://localhost:5071/api/v1/post/all/0/10", auth);
+      if (!d.ok) {
+        console.log(d, " error");
+      }
+      console.log(d);
+      const data = await d.json();
+      setData(data?.content || []);
+    }
+    getData();
+  }, []);
+  return (
+    <>
+      {data.map((item, index) => {
+        return (
+          <div className="col-lg-4 column-td-6" key={index}>
+            <div className="card-item">
+              <Link to={item.titleUrl} className="card-image-wrap">
+                <div className="card-image">
+                  <img
+                    src={item?.image || "https://via.placeholder.com/389x200"}
+                    className="card__img"
+                    alt="Place"
+                  />
+                  <span
+                    className={item.titleIcon ? "badge" : "badge badge-closed"}
+                  >
+                    {item?.bedge || "New"}
+                  </span>
+                  <span
+                    className="badge-toggle"
+                    data-toggle="tooltip"
+                    data-placement="bottom"
+                    title="22 Likes"
+                  >
+                    <FiHeart />
+                  </span>
+                </div>
+              </Link>
+              <div className="card-content-wrap">
+                <div className="card-content">
+                  <Link to={item.title}>
+                    <h5 className="card-meta">
+                      <span>{item.cardTypeIcon}</span> {item.category}
+                    </h5>
+                    <h4 className="card-title">
+                      {item.title}
+                      <i>{item.titleIcon}</i>
+                    </h4>
+                    <p className="card-sub">{item.location || "Location"}</p>
+                  </Link>
+                  <a href={item.authorUrl} className="author-img">
+                    <img
+                      src={item?.author || "https://via.placeholder.com/50"}
+                      alt="author-img"
+                    />
+                  </a>
+                  <ul className="info-list padding-top-20px">
+                    <li>
+                      <span className="la d-inline-block">
+                        <FiDollarSign />
+                      </span>{" "}
+                      {item.sellingPrice}
+                    </li>
+                    {/* <li>
+                      <span className="la d-inline-block">
+                        <IoIosLink />
+                      </span>{" "}
+                      <a href={item.websiteUrl}>{item.website}</a>
+                    </li> */}
+                    <li>
+                      <span className="la d-inline-block">
+                        <FaRegCalendarCheck />
+                      </span>{" "}
+                      {item.updatedOn}
+                    </li>
+                  </ul>
+                </div>
+                <div className="rating-row">
+                  <div className="rating-rating">
+                    {item?.ratings?.map((rating, index) => {
+                      return <span key={index}>{rating}</span>;
+                    })}
+                    <span className="rating-count">{item.ratingNum}</span>
+                  </div>
+                  <div className="listing-info">
+                    <ul>
+                      <li>
+                        <span className="info__count">
+                          <AiOutlineEye />
+                        </span>{" "}
+                        {item.view}
+                      </li>
+                      <li>
+                        <span
+                          className="info__save"
+                          data-toggle="tooltip"
+                          data-placement="top"
+                          title="Bookmark"
+                        >
+                          <FiHeart />
+                        </span>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export default PlaceGrid;
