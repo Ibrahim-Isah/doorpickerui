cbad86ae922068f5f597fa353ae4cd1a5c3c6678
Babafemi <babaphemy@gmail.com>
2021-12-16 00:40:02 -0500

    location

--- src/components/addlisting/AddPrice.js
+++ src/components/addlisting/AddPrice.js
@@ -1,60 +1,94 @@
-import React from 'react';
-import { FiEdit } from 'react-icons/fi'
-import { FaDollarSign } from 'react-icons/fa'
-import { BsPencil } from 'react-icons/bs'
+import React, { useState } from "react";
+import { FaDollarSign } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 function AddPrice() {
-    return (
-        <>
-            <div className="billing-form-item">
-                <div className="billing-title-wrap">
-                    <h3 className="widget-title pb-0">Pricing</h3>
-                    <div className="title-shape margin-top-10px"></div>
+  const [price, setPrice] = useState(0);
+  const [firm, setFirm] = useState(false);
+  const [terms, setTerms] = useState(false);
+  const _done = () => {
+    console.log(price, firm, terms);
+  };
+  return (
+    <>
+      <div className="billing-form-item">
+        <div className="billing-title-wrap">
+          <h3 className="widget-title pb-0">Price</h3>
+          <div className="title-shape margin-top-10px"></div>
+        </div>
+        <div className="billing-content">
+          <div className="contact-form-action">
+            <form method="post">
+              <div className="row">
+                <div className="col-lg-12">
+                  <div className="custom-checkbox d-block mr-0">
+                    <input
+                      type="checkbox"
+                      id="firm"
+                      checked={firm}
+                      onChange={() => setFirm(!firm)}
+                    />
+                    <label htmlFor="firm">Price is not negotiable</label>
+                  </div>
+
+                  {/* <div className="custom-checkbox d-block mr-0">
+                    <input
+                      type="checkbox"
+                      id="firm"
+                      checked={firm}
+                      onChange={() => setFirm(!firm)}
+                    />
+                    <label htmlFor="privacy">Price is not negotiable</label>
+                  </div> */}
                 </div>
-                <div className="billing-content">
-                    <div className="contact-form-action">
-                        <form method="post">
-                            <div className="row">
-                                <div className="col-lg-6">
-                                    <div className="input-box">
-                                        <label className="label-text">Title</label>
-                                        <div className="form-group">
-                                            <span className="la form-icon">
-                                                <FiEdit />
-                                            </span>
-                                            <input className="form-control" type="text" name="text" placeholder="Title" />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-lg-6">
-                                    <div className="input-box">
-                                        <label className="label-text">Price</label>
-                                        <div className="form-group">
-                                            <span className="la form-icon">
-                                                <FaDollarSign />
-                                            </span>
-                                            <input className="form-control" type="text" name="text" placeholder="USD Price" />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-lg-12">
-                                    <div className="input-box">
-                                        <label className="label-text">Description</label>
-                                        <div className="form-group mb-0">
-                                            <span className="la form-icon">
-                                                <BsPencil />
-                                            </span>
-                                            <textarea className="message-control form-control" name="message" placeholder="Write description"></textarea>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
+                <div className="col-lg-12">
+                  <div className="input-box">
+                    <label className="label-text">Price</label>
+                    <div className="form-group">
+                      <span className="la form-icon">
+                        <FaDollarSign />
+                      </span>
+                      <input
+                        className="form-control"
+                        type="number"
+                        name="price"
+                        placeholder="NGN Price"
+                        onChange={(e) => setPrice(e.target.value)}
+                        value={price}
+                      />
                     </div>
+                  </div>
                 </div>
-            </div>
-        </>
-    );
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div className="billing-form-item p-0 border-0 mb-0 shadow-none">
+        <div className="billing-content p-0">
+          <div className="custom-checkbox d-block mr-0">
+            <input
+              type="checkbox"
+              id="terms"
+              checked={terms}
+              onChange={() => setTerms(!terms)}
+            />
+            <label htmlFor="terms">
+              I Agree to DoorPicker's
+              <Link to="#" className="color-text">
+                Terms of Services
+              </Link>
+            </label>
+          </div>
+          <div className="btn-box mt-4">
+            <button onClick={_done} className="theme-btn border-0">
+              submit picket
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default AddPrice;
--- src/components/addlisting/GeneralInfo.js
+++ src/components/addlisting/GeneralInfo.js
@@ -1,117 +1,185 @@
-import React from 'react';
-import { BsPencilSquare, BsQuestion, BsPencil } from 'react-icons/bs'
-import { AiOutlineTags } from 'react-icons/ai'
+import React, { useContext, useState } from "react";
+import { Button } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { BsPencilSquare, BsPencil } from "react-icons/bs";
 import Select from "react-select";
+import { UserContext } from "../../context/UserProvider";
+import * as cats from "../../utils/category.json";
 
-const state = {
-    title: 'General Information',
-    selectedCatOp: null,
-    categories: [
-        {
-            value: 0,
-            label: 'Select a category'
-        },
-        {
-            value: 2,
-            label: 'Shops'
-        },
-        {
-            value: 3,
-            label: 'Hotels'
-        },
-        {
-            value: 4,
-            label: 'Restaurants'
-        },
-        {
-            value: 5,
-            label: 'Fitness'
-        },
-        {
-            value: 6,
-            label: 'Travel'
-        },
-        {
-            value: 7,
-            label: 'Salons'
-        },
-        {
-            value: 8,
-            label: 'Event'
-        },
-        {
-            value: 9,
-            label: 'Business'
-        },
-    ]
-}
+const ca = cats.category.map((c) => {
+  return { value: c.cat, label: c.cat };
+});
+const condit = cats.condition.map((cc) => {
+  return { value: cc, label: cc };
+});
+
 function GeneralInfo() {
-    return (
-        <>
-            <div className="billing-form-item">
-                <div className="billing-title-wrap">
-                    <h3 className="widget-title pb-0">{state.title}</h3>
-                    <div className="title-shape margin-top-10px"></div>
-                </div>
-                <div className="billing-content">
-                    <div className="contact-form-action">
-                        <form method="post">
-                            <div className="row">
-                                <div className="col-lg-6">
-                                    <div className="input-box">
-                                        <label className="label-text">Listing Title</label>
-                                        <div className="form-group">
-                                            <span className="la form-icon">
-                                                <BsPencilSquare />
-                                            </span>
-                                            <input className="form-control" type="text" name="name" placeholder="Enter your listing title" />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-lg-6">
-                                    <div className="input-box">
-                                        <label className="label-text d-flex align-items-center ">Keywords
-                                            <i className="la tip ml-1" data-toggle="tooltip" data-placement="top" title="Maximum of 15 keywords related with your business">
-                                                <BsQuestion />
-                                            </i>
-                                        </label>
-                                        <div className="form-group">
-                                            <span className="la form-icon">
-                                                <AiOutlineTags />
-                                            </span>
-                                            <input className="form-control" type="text" name="name" placeholder="Keywords should be separated by commas" />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-lg-12">
-                                    <div className="input-box">
-                                        <label className="label-text">Description</label>
-                                        <div className="form-group">
-                                            <span className="la form-icon">
-                                                <BsPencil />
-                                            </span>
-                                            <textarea className="message-control form-control" name="message" placeholder="Write your listing description"></textarea>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-lg-12">
-                                    <div className="input-box">
-                                        <label className="label-text">Category</label>
-                                        <div className="form-group mb-0">
-                                            <Select
-                                                placeholder="Select a Category"
-                                                options={state.categories}
-                                            />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
+  const [state, dispatch] = useContext(UserContext);
+  const [sub, setSub] = useState(null);
+  const [t, setT] = useState([]);
+  const [res, setRes] = useState(null);
+  const [cond, setCondition] = useState("");
+  const getSub = (cat) => {
+    setRes({ cat });
+    const sel = cats.category.find((c) => c.cat === cat);
+    const res = sel?.items?.map((s) => {
+      return { value: s.name, label: s.name };
+    });
+    setSub(res || []);
+  };
+  const _sub = (su) => {
+    const st = cats.category
+      .find((a) => a.cat === res.cat)
+      .items.find((b) => b.name === su);
+    const ms = st.subcategory.map((sms) => {
+      return { value: sms, label: sms };
+    });
+    setT(ms);
+    const ba = { ...res, subcat: su };
+    setRes(ba);
+  };
+  const _type = (ty) => {
+    const f = { ...res, type: ty };
+    setRes(f);
+  };
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const _done = (data) => {
+    console.log(res, " resident", cond);
+    console.log(data, " dat");
+  };
+  return (
+    <>
+      <div className="billing-form-item">
+        <div className="billing-title-wrap">
+          <h3 className="widget-title pb-0">General Information</h3>
+          <div className="title-shape margin-top-10px"></div>
+        </div>
+        <div className="billing-content">
+          <div className="contact-form-action">
+            <form method="post">
+              <div className="row">
+                <div className="col-lg-12">
+                  <div className="input-box">
+                    <label className="label-text">Listing Title</label>
+                    <div className="form-group">
+                      <span className="la form-icon">
+                        <BsPencilSquare />
+                      </span>
+                      <input
+                        className="form-control"
+                        type="text"
+                        name="name"
+                        placeholder="Enter your listing title"
+                        {...register("title")}
+                      />
                     </div>
+                  </div>
                 </div>
-            </div>
-        </>
-    );
+                {/* <div className="col-lg-6">
+                  <div className="input-box">
+                    <label className="label-text d-flex align-items-center ">
+                      Keywords
+                      <i
+                        className="la tip ml-1"
+                        data-toggle="tooltip"
+                        data-placement="top"
+                        title="Maximum of 15 keywords related with your business"
+                      >
+                        <BsQuestion />
+                      </i>
+                    </label>
+                    <div className="form-group">
+                      <span className="la form-icon">
+                        <AiOutlineTags />
+                      </span>
+                      <input
+                        className="form-control"
+                        type="text"
+                        name="name"
+                        placeholder="Keywords should be separated by commas"
+                      />
+                    </div>
+                  </div>
+                </div> */}
+                <div className="col-lg-12">
+                  <div className="input-box">
+                    <label className="label-text">Description</label>
+                    <div className="form-group">
+                      <span className="la form-icon">
+                        <BsPencil />
+                      </span>
+                      <textarea
+                        className="message-control form-control"
+                        name="message"
+                        placeholder="Write your listing description"
+                        {...register("description")}
+                      ></textarea>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-6">
+                  <div className="input-box">
+                    <label className="label-text">Category</label>
+                    <div className="form-group mb-0">
+                      <Select
+                        placeholder="Select a Category"
+                        options={ca}
+                        onChange={(e) => getSub(e.value)}
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-6">
+                  <div className="input-box">
+                    <label className="label-text">Subcategory</label>
+                    <div className="form-group mb-0">
+                      <Select
+                        placeholder="Select a Subcategory"
+                        options={sub}
+                        onChange={(e) => _sub(e.value)}
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-6">
+                  <div className="input-box">
+                    <label className="label-text">Type</label>
+                    <div className="form-group mb-0">
+                      <Select
+                        placeholder="Select type"
+                        options={t}
+                        onChange={(e) => _type(e.value)}
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-6">
+                  <div className="input-box">
+                    <label className="label-text">Condition</label>
+                    <div className="form-group mb-0">
+                      <Select
+                        placeholder="Select a Condition"
+                        options={condit}
+                        onChange={(e) => setCondition(e.value)}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </form>
+            <Button onClick={handleSubmit(_done)}>Done</Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default GeneralInfo;
