a372c08f34b0f8c85d33bde02961efeb36d22c34
Emmanuel <myoluwatoyin@gmail.com>
2021-12-25 20:59:43 +0100

    change

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,9 +1,9 @@
 let BASE_URL = "https://doorpicker.herokuapp.com/api/v1/";
-if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  BASE_URL = "http://localhost:5071/api/v1/";
-} else {
-  BASE_URL = "https://doorpicker.herokuapp.com/api/v1/";
-}
+// if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+//  BASE_URL = "http://localhost:5071/api/v1/";
+// } else {
+//  BASE_URL = "https://doorpicker.herokuapp.com/api/v1/";
+// }
 const authKey = process.env.REACT_APP_AUTH;
 export const PostSettings = (obj) => {
   return {
