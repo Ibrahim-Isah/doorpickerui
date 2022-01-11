9bc146e2b4528b5f27811217d244f19aa7e20013
Babafemi <babaphemy@gmail.com>
2022-01-10 23:15:50 -0600

    reducing banner height

---
+++ src/components/common/BreadcrumLte.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BreadcrumLte = ({ CurrentPgTitle, MenuPgTitle, img }) => {
+  return (
+    <>
+      <section className="breadcrumb-area"></section>
+    </>
+  );
+};
+
+export default BreadcrumLte;
