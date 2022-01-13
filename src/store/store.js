a5e5cc011ae7cffa59ae1b5806e2d703d2d663f6
Babafemi <babaphemy@gmail.com>
2022-01-13 14:06:07 -0600

    removing ununsed banner

--- src/store/store.js
+++ src/store/store.js
@@ -123,7 +123,6 @@ import hiw3videoimg from "../assets/images/video-img2.jpg";
 import { FcLineChart } from "react-icons/fc";
 import dreamimg from "../assets/images/img6.jpg"; // 263*165
 import hiw5videoimg from "../assets/images/video-img3.jpg";
-import slicevideo from "../assets/video/slice-video.m4v";
 import flag10 from "../assets/images/flag10.png";
 import flag11 from "../assets/images/flag11.png";
 import flag12 from "../assets/images/flag12.jpg";
@@ -406,7 +405,7 @@ const sectiondata = {
       sectitle: "Find The Best Places To Be",
       seccontent:
         "Discover the best places to stay, eat, shop & visit the city nearest to you.",
-      videobgsrc: slicevideo,
+      videobgsrc: "",
     },
   },
   categories: {
